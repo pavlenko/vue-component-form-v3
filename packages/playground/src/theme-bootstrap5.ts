43df4c4b445510d02799c4e7d87f1af04c5262a3
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-02-28 16:24:49 +0200

    add separate enpoint for theme

---
+++ packages/playground/src/theme-bootstrap5.ts
@@ -0,0 +1,10 @@
+import 'bootstrap-4/scss/bootstrap.scss'
+
+import {createApp} from 'vue'
+import {VFormPlugin, VFormPluginOptions, VFormPluginTheme} from "@pe/vue-component-form-v3/src/main";
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(VFormPlugin, <VFormPluginOptions>{theme: VFormPluginTheme.BOOTSTRAP_5});
+app.mount('#app')
